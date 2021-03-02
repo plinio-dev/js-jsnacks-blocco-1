c70206361ba4e6939942cc444bef9f8cfce66135
Tommaso P <tommaso.plinio@gmail.com>
2021-03-02 11:17:08 +0100

    snack 1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.*/
+
+var numero1 = prompt ("inserisci un numero")
+if ( isNaN (km) ) {
+  km = parseInt(prompt ("inserire un numero!"));
+var numero2 = prompt ("inserisci un numero")
+if ( isNaN (km) ) {
+  km = parseInt(prompt ("inserire un numero!"));
+
+
+
+if ( numero1 > numero2) {
+
+  console.log("il numero 1 è maggiore del numero 2");
+} else {
+  console.log("il numero 2 è maggiore del numero 1");
+}
